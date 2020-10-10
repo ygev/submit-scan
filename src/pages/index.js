086ec4f4f7a6925f7bd95ee68a6e05a336c63689
Yana <yanacommit@wizwar.net>
2020-10-09 23:37:47 -0700

    Set up grid structure for desktop and mobile.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,27 +11,29 @@ class IndexPage extends React.Component {
     return (
       <>
         <SEO title="Submit Scan" />
-        <Hero />
-        <aside>
-          <Input criterion="Bodies Reported" line="true"/>
-          <Input criterion="Emergencies Called" line="true"/>
-          <Input criterion="Tasks Completed" line="true"/>
-          <Input criterion="All Tasks Completed" line="true"/>
-          <Input criterion="Sabotages Fixed" line="true"/>
-          <Input criterion="Impostor Kills" line="true"/>
-          <Input criterion="Times Murdered" line="true"/>
-          <Input criterion="Times Ejected" line="true"/>
-          <Input criterion="Crewmate Streak" line="true"/>
-          <Input criterion="Times Impostor" line="true"/>
-          <Input criterion="Times Crewmate" line="true"/>
-          <Input criterion="Games Started" line="true"/>
-          <Input criterion="Games Finished" line="true"/>
-          <Input criterion="Impostor Vote Wins" line="true"/>
-          <Input criterion="Impostor Kill Wins" line="true"/>
-          <Input criterion="Impostor Sabotage Wins" line="true"/>
-          <Input criterion="Crewmate Vote Wins" line="true"/>
-          <Input criterion="Crewmate Task Wins"/>
-        </aside>
+        <div className="flexer">
+          <Hero />
+          <aside>
+            <Input criterion="Bodies Reported" line="true"/>
+            <Input criterion="Emergencies Called" line="true"/>
+            <Input criterion="Tasks Completed" line="true"/>
+            <Input criterion="All Tasks Completed" line="true"/>
+            <Input criterion="Sabotages Fixed" line="true"/>
+            <Input criterion="Impostor Kills" line="true"/>
+            <Input criterion="Times Murdered" line="true"/>
+            <Input criterion="Times Ejected" line="true"/>
+            <Input criterion="Crewmate Streak" line="true"/>
+            <Input criterion="Times Impostor" line="true"/>
+            <Input criterion="Times Crewmate" line="true"/>
+            <Input criterion="Games Started" line="true"/>
+            <Input criterion="Games Finished" line="true"/>
+            <Input criterion="Impostor Vote Wins" line="true"/>
+            <Input criterion="Impostor Kill Wins" line="true"/>
+            <Input criterion="Sabotage Wins" line="true"/>
+            <Input criterion="Crewmate Vote Wins" line="true"/>
+            <Input criterion="Crewmate Task Wins"/>
+          </aside>
+        </div>
         <Footer />
       </>
     );
