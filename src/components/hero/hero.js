720bb09df20e643c1ca850f89aa9c99c96af7fc1
Yana <yanacommit@wizwar.net>
2020-10-09 18:26:43 -0700

    Set up starter and define CSS variables.

---
+++ src/components/hero/hero.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../global.css";
+import "../reset.css";
+import "./hero.css"
+import "../fonts/type.css"
+
+class Hero extends React.Component {
+    render() {
+        return (
+    <>
+        <h1>Lead</h1>
+        <h2>Head</h2>
+        <h3>Button</h3>
+        <h4>inputLabel</h4>
+        <p>inputField</p>
+    </>
+  );
+}
+}
+
+export default Hero
