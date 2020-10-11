03baf61385709d7b05db5af519115a07fb75cc97
Yana <yanacommit@wizwar.net>
2020-10-10 19:35:04 -0700

    Add glowing input validation and transition.

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -4,12 +4,26 @@ import "../reset.css";
 import "./input.css"
 import "../fonts/type.css"
 
+
+function colorGreen() {
+    for (var i = 0; i < 18; i++) {
+        var inputs = document.getElementsByTagName('input')[i];
+        if(inputs.checkValidity() === true){
+            console.log("onBlur Running")
+            document.getElementsByClassName("input__wrapper")[i].classList.add("input__wrapper--filled");
+            document.getElementsByClassName("input--default")[i].classList.add("input--filled");
+            document.getElementsByClassName("input__line")[i].classList.add("glowOuter");
+        }
+    }
+}
+
 export default (props) => (
-            <>  <div className="inputLine__wrapper">
+            <>  
+            <div className="inputLine__wrapper">
                     <div className="input__wrapper">
                         <label for="criterion"><h4>{props.criterion}</h4></label>
-                        <input className="inputField input--default" type="text" name="criterion" required
-                            minlength="1" maxlength="5" placeholder="XXXXX"></input>
+                        <input onBlur={colorGreen} className="inputField input--default" type="number" name="criterion" required
+                            minLength="1" maxLength="5" placeholder="XXXXX"></input>
                     </div>
                     <div className="input__line">
                     </div>
