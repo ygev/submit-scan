fed165f043918bd188bebd2ae11d2ebacef17261
Yana <yanacommit@wizwar.net>
2020-10-21 09:40:18 -0700

    Slap some particles.js stars in the background.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Hero  from "../components/hero/hero"
 import Input  from "../components/input/input"
 import Footer from "../components/footer/footer"
 import SEO from "../components/seo"
+import Particles from 'react-particles-js';
 import "./index.css"
 
 class IndexPage extends React.Component {
@@ -44,9 +45,55 @@ class IndexPage extends React.Component {
           </aside>
         </div>
         <Footer />
-      </>
-    );
-  }
-}
+        <div id="particles-js">
+        <Particles
+                params={{
+                  "particles": {
+                      "number": {
+                          "value": 60,
+                          "density": {
+                              "enable": true,
+                              "value_area": 1500
+                          }
+                      },
+                      "line_linked": {
+                          "enable": true,
+                          "opacity": 0.02
+                      },
+                      "move": {
+                          "direction": "right",
+                          "speed": 0.05
+                      },
+                      "size": {
+                          "value": 1
+                      },
+                      "opacity": {
+                          "anim": {
+                              "enable": true,
+                              "speed": 1,
+                              "opacity_min": 0.05
+                          }
+                      }
+                  },
+                  "interactivity": {
+                      "events": {
+                          "onclick": {
+                              "enable": true,
+                              "mode": "push"
+                          }
+                      },
+                      "modes": {
+                          "push": {
+                              "particles_nb": 1
+                          }
+                      }
+                  },
+                  "retina_detect": true
+              }} />
+              </div>
+            </>
+          );
+        }
+      }
 
 export default IndexPage;
