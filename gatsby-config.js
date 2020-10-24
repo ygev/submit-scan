ebe849a50c3efd5992f6d1ec7f1dd1df61d7de71
Yana <yanacommit@wizwar.net>
2020-10-24 10:32:14 -0700

    Update metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Among Us Stats`,
+        short_name: `stats`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,
