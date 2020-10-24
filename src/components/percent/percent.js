493a5d836f29dd5cf14973c6a68fcac13362decb
TimWilliams614 <timwilliams614@gmail.com>
2020-10-23 23:48:44 -0700

    tied percentages to components

--- src/components/percent/percent.js
+++ src/components/percent/percent.js
@@ -5,15 +5,15 @@ import "./percent.css"
 import "../fonts/type.css"
 
 export default (props) => (
-            <>
-                <section className="percent__container">
-                    <div className="percent__circle percent__circle--num">
-                        <h4 className="percent__num">{props.percentNum}</h4>
-                    </div>
-                    <h4 className="percent__txt">{props.percentTxt}</h4>
-                    <div className="percent__circle">
-                        <img src={props.percentImg} alt="" className="percent__img"/>
-                    </div>
-                </section>
-            </>
-        );
+    <>
+        <section className="percent__container">
+            <div className="percent__circle percent__circle--num">
+                <h4 id={props.id} className="percent__num">{props.percentNum}</h4>
+            </div>
+            <h4 className="percent__txt">{props.percentTxt}</h4>
+            <div className="percent__circle">
+                <img src={props.percentImg} alt="" className="percent__img"/>
+            </div>
+        </section>
+    </>
+);
